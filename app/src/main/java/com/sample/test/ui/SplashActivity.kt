f369d3d8c44b68075051a50c0190fd66a30845a7
Sangili Pandian C <sangili.pandian@olamnet.com>
2019-11-23 21:17:33 +0530

    splash screen added

---
+++ app/src/main/java/com/sample/test/ui/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.sample.test.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import com.sample.test.R
+import com.sample.test.ui.base.BaseActivity
+import com.sample.test.ui.home.HomeActivity
+import com.sample.test.utils.Constants.mDelay
+
+/**
+ * Created by SangiliPandian C on 23-11-2019.
+ */
+class SplashActivity : BaseActivity() {
+
+    override val layoutResourceId = R.layout.activity_splash
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            moveToLoginPage()
+        }, mDelay)
+    }
+
+    private fun moveToLoginPage() {
+        val intent = Intent(this, HomeActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+        finish()
+    }
+}
--- app/src/main/java/com/sample/test/utils/Constants.kt
+++ app/src/main/java/com/sample/test/utils/Constants.kt
@@ -5,6 +5,7 @@ package com.sample.test.utils
  */
 object Constants {
     const val CONNECTIVITY_CHANGE = "android.net.conn.CONNECTIVITY_CHANGE"
+    const val mDelay = 2000L
     const val CONNECT_TIMEOUT = 20L
     const val READ_TIMEOUT = 20L
     const val CONTENT_TYPE = "Content-Type"
